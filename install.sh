eae0a1b01269b244d2fc0c99767e85df8275df40
Xavier Briand <xavierbriand@gmail.com>
2018-10-21 16:28:28 -0400

    Set powerline-multiline as default bash theme

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh --silent
+
+sed -i ''  "s/BASH_IT_THEME='bobby'/BASH_IT_THEME='powerline-multiline'/" ~/.bash_profile
